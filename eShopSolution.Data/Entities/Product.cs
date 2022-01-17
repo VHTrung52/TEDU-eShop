b133ccb73531ddbe44c3c18a597ebf769f60a303
VHTrung52 <vhtrung52@gmail.com>
2022-01-17 09:19:12 +0700

    migration database

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -15,5 +15,10 @@ namespace eShopSolution.Data.Entities
         public  int ViewCount { get; set; }
         public DateTime DateCreated { get; set; }
         public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+        public List<Cart> Carts { get; set; }
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }

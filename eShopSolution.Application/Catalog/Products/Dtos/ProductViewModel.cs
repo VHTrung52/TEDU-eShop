a451522792a5e79b75ed6a0ad86f382c139e58d6
VHTrung52 <vhtrung52@gmail.com>
2022-01-23 17:27:13 +0700

    feature/06_application_layer

---
+++ eShopSolution.Application/Catalog/Products/Dtos/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string Details { set; get; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+        public string SeoAlias { get; set; }
+        public string LanguageId { set; get; }
+    }
+}

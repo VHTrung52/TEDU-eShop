706c5155a16d9648f7f3c0d99af2aca46d15aac0
VHTrung52 <vhtrung52@gmail.com>
2022-04-17 18:08:38 +0700

    feature/22_product_list

---
+++ eShopSolution.AdminApp/Sevices/ProductApiClient.cs
@@ -0,0 +1,46 @@
+﻿using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Sevices
+{
+    public class ProductApiClient : BaseApiClient, IProductApiClient
+    {
+        public ProductApiClient(
+            IHttpClientFactory httpClientFactory,
+            IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
+            : base(httpClientFactory, configuration, httpContextAccessor)
+        {
+        }
+
+        public Task<ApiResult<bool>> DeleteProduct(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<ProductViewModel>> GetProductById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ApiResult<PagedResult<ProductViewModel>>> GetProductPagings(GetManageProductPagingRequest request)
+        {
+            return await GetAsync<ApiResult<PagedResult<ProductViewModel>>>("" +
+                "/api/products/paging?" +
+                $"pageIndex={request.PageIndex}" +
+                $"&pageSize={request.PageSize}" +
+                $"&keyWord={request.Keyword}" +
+                $"&languageId={request.LanguageId}");
+        }
+
+        public Task<ApiResult<bool>> UpdateProduct(Guid id, ProductUpdateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eShopSolution.BackendApi/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private const string UnexpectedServerErrorStr = "An Unexpected Server Error has occurred.";
+
+        protected ILogger<BaseController> Logger { get; }
+
+        protected BaseController(ILogger<BaseController> lgr)
+        {
+            Logger = lgr;
+        }
+
+        protected ObjectResult ServerError(string errMsg = null)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, errMsg ?? UnexpectedServerErrorStr);
+        }
+    }
+}
